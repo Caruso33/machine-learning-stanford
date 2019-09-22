7cb254b0116235799453b87cd1c04d0b18ffae4d
Tobias <tobias@appysphere.com>
2019-09-22 16:22:48 +0700

    Finish ex4

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -50,14 +50,10 @@ X = [ones(m, 1) X];
 %
 
 
-size(num_labels)
-size(all_theta)
-
-
 for i = 1:num_labels
 	initial_theta = zeros(n + 1, 1);
 
-        options = optimset('GradObj', 'on', 'MaxIter', 50);
+	options = optimset('GradObj', 'on', 'MaxIter', 50);
 	
 	[theta] = ...
         	fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), ...
